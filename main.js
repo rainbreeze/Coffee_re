d30b134ea8d48b2f4d502303534bd2e68d421e7a
rainbreeze <rainbreeze2@naver.com>
2024-08-30 15:39:44 +0900

    HEADER, VISUAL I

--- main.js
+++ main.js
@@ -14,3 +14,26 @@ inputEl.addEventListener('blur', function(){
     searchEl.classList.remove('focused');
     inputEl.setAttribute('placeholder', '');
 });
+
+const fadeEls = document.querySelectorAll('.visual .fade-out');
+fadeEls.forEach(function(fadeEl, index){
+    gsap.to(fadeEl, 1, {
+        opacity : 1,
+        delay : (index + 1) * .7
+    });
+});
+
+const badgesEl = document.querySelector('.badges');
+
+window.addEventListener('scroll', function() {
+    if(window.scrollY > 500) {
+        gsap.to(badgesEl, .6, {
+            opacity : 0
+        });
+    }
+    else{
+        gsap.to(badgesEl, .6, {
+            opacity : 1
+        });
+    }
+});
